860d8e69d1fa24ec2f8556c84bd51d3ce9d6940e
Armando Santos <armandosantosec@gmail.com>
2021-07-25 12:27:03 -0600

    Calculadora

--- app/src/main/java/com/aesc/worldskillsdemoapp1/MainActivity.kt
+++ app/src/main/java/com/aesc/worldskillsdemoapp1/MainActivity.kt
@@ -1,35 +1,143 @@
 package com.aesc.worldskillsdemoapp1
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import kotlinx.android.synthetic.main.fragment_primer.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity()/*, View.OnClickListener*/ {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var iden: String? = ""
+    private var clear: Boolean? = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
-        val navController = navHostFragment.navController
+        /*val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController*/
 /*        btnTest.setOnClickListener(this)
         btnTest2.setOnClickListener(this)*/
 
+//        Funcionalidad de los botones
+        initCalculadora()
+
     }
 
-    /*override fun onClick(v: View?) {
+    private fun initCalculadora() {
+        initButtons()
+    }
+
+    private fun initButtons() {
+        btnNo1.setOnClickListener(this)
+        btnNo2.setOnClickListener(this)
+        btnNo3.setOnClickListener(this)
+        btnNo4.setOnClickListener(this)
+        btnNo5.setOnClickListener(this)
+        btnNo6.setOnClickListener(this)
+        btnNo7.setOnClickListener(this)
+        btnNo8.setOnClickListener(this)
+        btnNo9.setOnClickListener(this)
+        btnNo0.setOnClickListener(this)
+        btnEq.setOnClickListener(this)
+        btnSuma.setOnClickListener(this)
+        btnResta.setOnClickListener(this)
+        btnMultplicacion.setOnClickListener(this)
+        btnDivicion.setOnClickListener(this)
+    }
+
+    private fun printScreen(value: String) {
+        val screenValue = etPantalla.text.toString()
+        if (screenValue == "0.00") {
+            etPantalla.setText(value)
+        } else {
+            val screen = screenValue + value
+            etPantalla.setText(screen)
+        }
+    }
+
+    override fun onClick(v: View?) {
         val idButton = v?.id
 
+        if (clear!!) {
+            etPantalla.setText("")
+            clear = false
+        }
         //switch
         when (idButton) {
-            (R.id.btnTest) -> {
-                val cajaDeTexto = etIngresarTexto.text.toString()
-                tv_name.text = "from button 1 $cajaDeTexto"
+            R.id.btnNo0 -> {
+                printScreen("0")
             }
-            R.id.btnTest2 -> {
-                val cajaDeTexto = etIngresarTexto.text.toString()
-                tv_name.text = "from button 2 $cajaDeTexto"
+            R.id.btnNo1 -> {
+                printScreen("1")
+            }
+            R.id.btnNo2 -> {
+                printScreen("2")
+            }
+            R.id.btnNo3 -> {
+                printScreen("3")
+            }
+            R.id.btnNo4 -> {
+                printScreen("4")
+            }
+            R.id.btnNo5 -> {
+                printScreen("5")
+            }
+            R.id.btnNo6 -> {
+                printScreen("6")
+            }
+            R.id.btnNo7 -> {
+                printScreen("7")
+            }
+            R.id.btnNo8 -> {
+                printScreen("8")
+            }
+            R.id.btnNo9 -> {
+                printScreen("9")
+            }
+            R.id.btnEq -> {
+                val screen = etPantalla.text.toString()
+                operar(screen)
+            }
+            R.id.btnSuma -> {
+                iden = "S"
+                printScreen("+")
+            }
+            R.id.btnResta -> {
+                iden = "R"
+                printScreen("-")
+            }
+            R.id.btnMultplicacion -> {
+                iden = "M"
+                printScreen("*")
+            }
+            R.id.btnDivicion -> {
+                iden = "D"
+                printScreen("/")
             }
         }
-    }*/
+    }
+
+    private fun operar(screen: String) {
+        val pattern = Regex("""\s*[-\+*]\s*""")
+        val founds = screen.split(pattern)
+        etPantalla.setText("0.00")
+        when (iden) {
+            "S" -> {
+                val result = (founds[0].toInt() + founds[1].toInt())
+                printScreen(result.toString())
+            }
+            "R" -> {
+                val result = (founds[0].toInt() - founds[1].toInt())
+                printScreen(result.toString())
+            }
+            "M" -> {
+                val result = (founds[0].toInt() * founds[1].toInt())
+                printScreen(result.toString())
+            }
+            "D" -> {
+                val result = (founds[0].toInt() / founds[1].toInt())
+                printScreen(result.toString())
+            }
+        }
+        clear = true
+    }
 }
--- app/src/main/java/com/aesc/worldskillsdemoapp1/fragments/SegundoFragment.kt
+++ app/src/main/java/com/aesc/worldskillsdemoapp1/fragments/SegundoFragment.kt
@@ -24,14 +24,12 @@ class SegundoFragment : Fragment(), View.OnClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_segundo, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        mNavController = Navigation.findNavController(view);
+        mNavController = Navigation.findNavController(view)
         btnPrimeraActivity.setOnClickListener(this)
     }
 
